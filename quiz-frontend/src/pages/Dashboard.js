c3d9193a9c2f95d36bb5ab11345ce99a68b13ee7
Kumar Sachin <sachinkc4456@gmail.com>
2025-03-31 14:08:25 +0530

    final

--- quiz-frontend/src/pages/Dashboard.js
+++ quiz-frontend/src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("wss://13.203.101.148:5000", {
+const socket = io("https://quiz-app-so3y.onrender.com", {
   transports: ["websocket"], // Force WebSocket connection (no polling)
   withCredentials: true, // If you need credentials (cookies, etc.)
 });
--- quiz-frontend/src/pages/Leaderboard.js
+++ quiz-frontend/src/pages/Leaderboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { io } from "socket.io-client";
-const socket = io("wss://13.203.101.148:5000", {
+const socket = io("https://quiz-app-so3y.onrender.com", {
   transports: ["websocket"], // Force WebSocket connection (no polling)
   withCredentials: true, // If you need credentials (cookies, etc.)
 });
@@ -13,7 +13,7 @@ function Leaderboard() {
   useEffect(() => {
     // Fetch initial leaderboard data
     axios
-      .get("http://13.203.101.148:5000/api/quiz/leaderboard")
+      .get("https://quiz-app-so3y.onrender.com/api/quiz/leaderboard")
       .then((res) => {
         setLeaderboard(res.data);
         setLoading(false);
